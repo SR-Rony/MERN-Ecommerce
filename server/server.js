91576a5324e49d3da09eb4369246bb85e954b7ac
SR Rony <sr@sr.rony>
2024-02-06 12:25:15 +0600

    error meddleware add

--- server/server.js
+++ server/server.js
@@ -5,11 +5,28 @@ const PORT =8000
 
 
 app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 
 app.get("/",(req,res)=>{
     res.status(200).send('welcome to my server')
 })
+
+// client error
+app.use((req,res,next)=>{
+    res.status(404).send({
+        message:"404 page is not found"
+    })
+})
+
+// server error
+app.use((err,req,res,next)=>{
+    res.status(500).send({
+        message:"server is brock"
+    })
+})
+
 
 app.listen(PORT,()=>{
     console.log(`my server is running at http://localhost:${PORT}`);
